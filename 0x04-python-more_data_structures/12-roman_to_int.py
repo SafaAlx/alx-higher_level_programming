ca062b80790be0f3d700d2cafb066c83e5ccf518
SafaAlx <safae.mimoune@gmail.com>
2023-08-16 10:38:49 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -14,7 +14,7 @@ def roman_to_int(roman_string):
     if not roman_string:
         return 0
 
-    if not isinstance(roman_string):
+    if not isinstance(roman_string, str):
         return 0
 
     rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
@@ -26,7 +26,7 @@ def roman_to_int(roman_string):
 
     for ch in roman_string:
         for r_num in list_keys:
-            if r_num = ch:
+            if r_num == ch:
                 if rom_n.get(ch) <= last_rom:
                     num += to_subtract(list_num)
 
