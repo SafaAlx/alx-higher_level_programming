a420a26d56196ba66a64eea824e33a5d5d38ea9b
SafaAlx <safae.mimoune@gmail.com>
2023-08-29 03:40:16 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+class Square:
+    """Represent a square."""
+
+    def__init__(self, size):
+        """initialize a new Square.
+
+        Args:
+        size (int): The size of the new square.
+        """
+        self.__size = size
