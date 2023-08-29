6b03c88fc0ea4b89fb3b2b6e3a3ed49d6649fb59
SafaAlx <safae.mimoune@gmail.com>
2023-08-28 17:29:12 -0700

    4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python3
-list_division = __import__('4-list_division').list_division
-my_|_1 = [10, 8, 4, 4]
-my_|_2 = [2, 4, 4]
-result = list_division(my_|_1, my_|_2, max(len(my_|_1), len(my_|_2)))
-print(result)
 
-print("--")
 
-my_|_1 = [10, 8, 4, 4]
-my_|_2 = [2, 0, "H", 2, 7]
-result = list_division(my_|_1, my_|_2, max(len(my_|_1), len(my_|_2)))
-print(result)
+def list_division(my_list_1, my_list_2, list_length):
+    """Divides two lists element by element.
+
+    Args:
+        my_list_1 (list): The first list.
+        my_list_2 (list): The second list.
+        list_length (int): The number of elements to divide.
+
+    Returns:
+        A new list of length list_length containing all the divisions.
+    """
+    new_list = []
+    for i in range(0, list_length):
+        try:
+            div = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            div = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            div = 0
+        except IndexError:
+            print("out of range")
+            div = 0
+        finally:
+            new_list.append(div)
+    return (new_list)
 
