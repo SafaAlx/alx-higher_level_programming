d62799dbae1844b37d5cc76a685898c6f2a51568
SafaAlx <safae.mimoune@gmail.com>
2024-01-28 13:53:30 +0000

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI GET "$1" | grep -i "Content-Length" | cut -d " " -f2
+<?php
+include 'lang.en.php';
+curl -sI GET "$1" | grep -i "Content-Length" | cut -d " " -f2;
+include 'header.php';
+?>
